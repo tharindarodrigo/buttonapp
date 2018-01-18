52f7bef54ae7543a65d24b91e4024a9203896cd6
tharidnarodrigo <tharindarodrigo@gmail.com>
2018-01-18 17:51:56 +0530

    modified

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,7 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        'buttons/{id}',
+        'button/{id}',
 //        'buttonsPressed'
     ];
 }
