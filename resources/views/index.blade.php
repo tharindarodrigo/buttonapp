4203b4af6a3f0a9441ebf59056374a65ace36029
tharidnarodrigo <tharindarodrigo@gmail.com>
2018-02-15 13:39:03 +0530

    modified

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,7 +10,7 @@
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <!-- Bootstrap core CSS -->
-    <link href="{!! asset('css/bootstrap.min.css') !!}" rel="stylesheet">
+    {{--<link href="{!! asset('css/bootstrap.min.css') !!}" rel="stylesheet">--}}
     <!-- Material Design Bootstrap -->
     {{--<link href="{!! asset('css/mdb.min.css') !!}" rel="stylesheet">--}}
     <!-- Your custom styles (optional) -->
