5e6f40645e8722b8d0b814eb136373baca2e5687
tharidnarodrigo <tharindarodrigo@gmail.com>
2018-02-27 16:34:14 +0530

    modified

--- routes/web.php
+++ routes/web.php
@@ -24,12 +24,12 @@ Route::get('buttons', 'ButtonController@index');
 
 
 Route::any('button/log', function (\Illuminate\Http\Request $request){
-//    $x = Input::all();
+    $x = $request->all();
 //    return 'hello';
     $button = new \App\Button();
     $button->button_id = 100;
     $button->status = 1;
-    $button->data = \Illuminate\Support\Facades\Input::all();
+    $button->data = (String)($request->all());
 //    $button->data = empty($x) ? 'nothing' : json_encode($x);
 
     return $button->save();
